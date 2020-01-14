1f15408146f379e1e26c9ce3a220cf88ec3bafdd
Danny Allegrezza <danny@dannyallegrezza.com>
2020-01-13 23:15:33 -0500

    Stop motor from pulsing all the time
    With the `digitalWrite` function we simply need to turn off voltage to
    the 4 pins controlling the stepper motor.

--- lessons/5-stepper-motor/5-stepper-motor.ino
+++ lessons/5-stepper-motor/5-stepper-motor.ino
@@ -6,15 +6,32 @@ Stepper myStepper(stepsPerRevolution, D1, D2, D5, D6);
 
 void setup() {
 
-  // set the speed at 80 rpm:
+  // set the speed at 100 rpm:
   myStepper.setSpeed(100);
   Serial.begin(115200);
+  feed();
 }
 
 void loop() {
+
+}
+
+void feed() {
+  Serial.println("feeding!");
+  // danny: for debugging purposes, was just verifying that I could use the const values for 
+  // the digitalWrite calls below..
+  Serial.println(D1);
+  Serial.println(D2);
+  Serial.println(D5);
+  Serial.println(D6);
+  
   // step one revolution  in one direction:
-  Serial.println("clockwise");
   myStepper.step(stepsPerRevolution);
 
-  delay(3000);
+  digitalWrite(D1, LOW);
+  digitalWrite(D2, LOW);
+  digitalWrite(D5, LOW);
+  digitalWrite(D6, LOW);
+  
+  delay(5000);
 }
